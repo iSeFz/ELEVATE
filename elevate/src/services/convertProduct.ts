837dcb12fc1fd222a85782345d4ea708d1171f93
Adham-K-Fahmy <noorcool116@gmail.com>
2025-07-05 16:08:38 +0300

    bugfix

--- elevate/src/services/convertProduct.ts
+++ elevate/src/services/convertProduct.ts
@@ -1,4 +1,7 @@
 export function capitalizeProductName(name: string): string {
+    if(!name || typeof name !== 'string') {
+        return name;
+    }   
     return name
         .toLowerCase()
         .split(' ')
